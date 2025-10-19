1a560cf9cb120ea28c487c61f8017961f321c92d
BechirABIDI <starilax.echanges@gmail.com>
2025-10-19 20:55:19 +0000

    Oct 19, 2025, 10:55 PM

--- reseaumistral/setvar/vehicules/index.php
+++ reseaumistral/setvar/vehicules/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log');
+error_reporting(E_ALL);
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
