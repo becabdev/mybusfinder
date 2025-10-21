05ec372941de2d7ce001b51a63a80f4311c07f49
BechirABIDI <starilax.echanges@gmail.com>
2025-10-21 18:43:55 +0000

    Oct 21, 2025, 8:43 PM

--- zou/proxy-cors/proxy_vehpos.php
+++ zou/proxy-cors/proxy_vehpos.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$url = 'https://mybusfinder.fr/zou-vehpos/vehicle_positions.pb';
+$url = 'https://mybusfinder.fr/gtfsrt/zou-prox/vehicle_positions.pb';
 $data = file_get_contents($url);
 echo $data;
 ?>
