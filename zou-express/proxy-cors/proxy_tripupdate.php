2cc6b0ba8a41a7ebc21e802a004e0e33a79e8c37
BechirABIDI <starilax.echanges@gmail.com>
2025-10-21 20:29:47 +0000

    Oct 21, 2025, 10:29 PM

--- zou-express/proxy-cors/proxy_tripupdate.php
+++ zou-express/proxy-cors/proxy_tripupdate.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/region-sud-zou-proximite-gtfs-rt-trip-update?token=BBOTIEO7q6bWqPdMQPAFKyUgZ8IfuwDQTx8rQHyPHqY';
+$url = 'https://proxy.transport.data.gouv.fr/resource/region-sud-zou-express-gtfs-rt-trip-update?token=BBOTIEO7q6bWqPdMQPAFKyUgZ8IfuwDQTx8rQHyPHqY';
 $data = file_get_contents($url);
 echo $data;
 ?>
