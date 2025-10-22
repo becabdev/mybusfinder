e70a02b60d060acb7dedd751fbfc0c80e4c3354e
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:33:52 +0000

    Oct 22, 2025, 11:33 PM

--- sankeo/proxy-cors/proxy_servicealerts.php
+++ sankeo/proxy-cors/proxy_servicealerts.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-service-alert';
+$url = 'https://transport.data.gouv.fr/resources/82273';
 $data = file_get_contents($url);
 echo $data;
 ?>
