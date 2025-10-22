9dedda19250bb72d694605900916c9f918ebc4be
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 15:45:18 +0000

    Oct 22, 2025, 5:45 PM

--- stas/proxy-cors/proxy_geojson.php
+++ stas/proxy-cors/proxy_geojson.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://transport.data.gouv.fr/resources/conversions/64973/GeoJSON';
+$url = 'https://transport.data.gouv.fr/resources/conversions/81023/GeoJSON';
 $data = file_get_contents($url);
 echo $data;
 ?>
