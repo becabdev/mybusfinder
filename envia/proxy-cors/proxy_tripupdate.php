afa60bc06f0006530ac501bb1b87b128635e0cac
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:53:10 +0000

    Oct 22, 2025, 11:53 PM

--- envia/proxy-cors/proxy_tripupdate.php
+++ envia/proxy-cors/proxy_tripupdate.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://data.montpellier3m.fr/TAM_MMM_GTFSRT/TripUpdate.pb';
+$url = 'https://accm.plateforme-2cloud.com/api/gtfsrt/2.0/tripupdates/ACCM-RSUD-4552-8552-5459/bin';
 $data = file_get_contents($url);
 echo $data;
 ?>
