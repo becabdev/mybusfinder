9798238ed36b78666483ec6c1176d7e69f772ec9
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:34:25 +0000

    Oct 22, 2025, 11:34 PM

--- sankeo/proxy-cors/proxy_vehpos.php
+++ sankeo/proxy-cors/proxy_vehpos.php
@@ -1,7 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
-
-$url = 'https://mybusfinder.fr/gtfsrt/zou-exp/vehicle_positions.pb';
+$url = 'https://transport.data.gouv.fr/resources/82273';
 $data = file_get_contents($url);
 echo $data;
 ?>
