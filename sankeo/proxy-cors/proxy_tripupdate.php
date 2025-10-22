3d7320843ded5a9346ffc86b3598078830b0660d
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:33:56 +0000

    Oct 22, 2025, 11:33 PM

--- sankeo/proxy-cors/proxy_tripupdate.php
+++ sankeo/proxy-cors/proxy_tripupdate.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/region-sud-zou-express-gtfs-rt-trip-update?token=BBOTIEO7q6bWqPdMQPAFKyUgZ8IfuwDQTx8rQHyPHqY';
+$url = 'https://transport.data.gouv.fr/resources/82273';
 $data = file_get_contents($url);
 echo $data;
 ?>
