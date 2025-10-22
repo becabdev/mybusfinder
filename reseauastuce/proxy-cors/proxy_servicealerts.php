4e202317181078b13b1ed2f575bd7c7413845711
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 15:29:57 +0000

    Oct 22, 2025, 5:29 PM

--- reseauastuce/proxy-cors/proxy_servicealerts.php
+++ reseauastuce/proxy-cors/proxy_servicealerts.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-service-alert';
+$url = 'https://api.mrn.cityway.fr/dataflow/info-transport/download?provider=ASTUCE&dataFormat=gtfs-rt';
 $data = file_get_contents($url);
 echo $data;
 ?>
