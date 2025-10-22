68f1108facfe0fc2b3c49cb4aedd67e6ed5a2084
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:33:38 +0000

    Oct 22, 2025, 11:33 PM

--- sankeo/proxy-cors/proxy_geojson.php
+++ sankeo/proxy-cors/proxy_geojson.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://eur.mecatran.com/utw/ws/gtfsfeed/static/perpignan?apiKey=612f606b5e3b0a3e6e1f441a2c4a050f6a345b55';
+$url = 'https://transport.data.gouv.fr/resources/conversions/82900/GeoJSON';
 $data = file_get_contents($url);
 echo $data;
 ?>
