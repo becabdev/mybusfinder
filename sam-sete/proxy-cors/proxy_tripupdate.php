0fe0a9968b4b9c5e423ecb34f4df34d96a72bfb2
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 21:44:02 +0000

    Oct 22, 2025, 11:44 PM

--- sam-sete/proxy-cors/proxy_tripupdate.php
+++ sam-sete/proxy-cors/proxy_tripupdate.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://transport.data.gouv.fr/resources/82273';
+$url = 'https://sete.ceccli.com/gtfs/TripUpdates.pb';
 $data = file_get_contents($url);
 echo $data;
 ?>
