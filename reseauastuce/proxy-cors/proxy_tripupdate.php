f442b46608d763edb8fd0a3f5f283c2b9c89e8a5
BechirABIDI <starilax.echanges@gmail.com>
2025-10-22 15:38:04 +0000

    Oct 22, 2025, 5:38 PM

--- reseauastuce/proxy-cors/proxy_tripupdate.php
+++ reseauastuce/proxy-cors/proxy_tripupdate.php
@@ -2,49 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$urls = [
-    'https://gtfs.bus-tracker.fr/gtfs-rt/tcar/trip-updates',
-];
-
-$mergedData = [];
-
-foreach ($urls as $url) {
-    try {
-        $data = @file_get_contents($url);
-        
-        if ($data === false) {
-            error_log("Erreur lors de la récupération de : $url");
-            continue;
-        }
-        
-        $decoded = json_decode($data, true);
-        
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            error_log("Erreur JSON pour $url : " . json_last_error_msg());
-            continue;
-        }
-        
-        if (isset($decoded['entity'])) {
-            if (!isset($mergedData['entity'])) {
-                $mergedData = $decoded;
-            } else {
-                $mergedData['entity'] = array_merge(
-                    $mergedData['entity'],
-                    $decoded['entity']
-                );
-            }
-        } else {
-            $mergedData = array_merge_recursive($mergedData, $decoded);
-        }
-        
-    } catch (Exception $e) {
-        error_log("Exception pour $url : " . $e->getMessage());
-    }
-}
-
-if (isset($mergedData['header'])) {
-    $mergedData['header']['timestamp'] = time();
-}
-
-echo json_encode($mergedData, JSON_PRETTY_PRINT);
+$url = 'https://gtfs.bus-tracker.fr/gtfs-rt/tcar/trip-updates';
+$data = file_get_contents($url);
+echo $data;
 ?>
