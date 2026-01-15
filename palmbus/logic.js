6ca76602e51a70b2d0d846bac1dbeeb1a1115597
BechirABIDI <starilax.echanges@gmail.com>
2026-01-15 16:36:55 +0100

    Parc RLA : ajout vehicules

--- palmbus/logic.js
+++ palmbus/logic.js
@@ -1080,7 +1080,7 @@ async function changeColorBkg(selectedTheme = null) {
     const theme = selectedTheme || savedTheme;
 
     const baseColor = themes[theme] || themes["default"];
-    const colorWithAlpha = `${baseColor}5c`;
+    const colorWithAlpha = `${baseColor}9c`;
 
     localStorage.setItem("theme", theme);
     localStorage.setItem("colorbkg", baseColor);
@@ -3006,7 +3006,7 @@ function updateMenuBtmColor(color, routeId) {
     StyleCleanupManager.check();
     
     requestAnimationFrame(() => {
-        menubtm.style.backgroundColor = `${color}5c`;
+        menubtm.style.backgroundColor = `${color}9c`;
         
         const textColor = TextColorUtils.getOptimal(color);
         const invert = textColor === '#1a1a1a' ? 'invert(1)' : 'invert(0)';
@@ -7219,7 +7219,7 @@ function createOrUpdateMinimalTooltip(markerId, shouldShow = true) {
                             if (menubtm) {
                                 const color = lineColors[line] || '#000000';
                                 lastActiveColor = color;
-                                menubtm.style.backgroundColor = `${color}5c`;
+                                menubtm.style.backgroundColor = `${color}9c`;
                                 
                                 const textColor = TextColorUtils.getOptimal(color);
                                 StyleManager.applyMenuStyle(textColor);
