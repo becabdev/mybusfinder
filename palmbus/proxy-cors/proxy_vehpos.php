750f0eb68bb4be186dacedc631ef1d31b1a2ab0a
BechirABIDI <starilax.echanges@gmail.com>
2026-02-06 23:14:53 +0100

    bug coorect

--- palmbus/proxy-cors/proxy_vehpos.php
+++ palmbus/proxy-cors/proxy_vehpos.php
@@ -2,11 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://www.data.gouv.fr/api/1/datasets/r/f209bfe9-6be2-499c-96b9-eff0c6c7e7ec';
+$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-vehicle-position';
 $data = file_get_contents($url);
 echo $data;
 ?>
-
-
-// fallback https://www.data.gouv.fr/api/1/datasets/r/f209bfe9-6be2-499c-96b9-eff0c6c7e7ec
-// on restore https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-vehicle-position
